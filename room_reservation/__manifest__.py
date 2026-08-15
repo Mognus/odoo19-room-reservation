69b46cdbf84f7f0f74aa1e4309408f9b72b3be3d
Mognus <magnusderkleine@gmail.com>
2026-08-15 22:55:20 +0200

    feat: add room_reservation module skeleton with manifest

---
+++ room_reservation/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    "name": "Room Reservations",
+    "summary": "Book meeting rooms with capacity checks and an approval workflow",
+    "version": "19.0.1.0.0",
+    "category": "Productivity",
+    "license": "LGPL-3",
+    "author": "Mognus",
+    "website": "https://github.com/Mognus/odoo19-room-reservation",
+    "depends": ["base", "mail"],
+    "data": [],
+    "application": True,
+    "installable": True,
+}
